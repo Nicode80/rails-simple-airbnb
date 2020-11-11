0cea9d3ac52b8776c813affe64c330afbb98d78b
Nicolas Constantin <nicolasconstantin@hotmail.fr>
2020-11-11 23:21:01 +0100

    edit done show started

---
+++ db/migrate/20201111210217_add_url_to_flats.rb
@@ -0,0 +1,5 @@
+class AddUrlToFlats < ActiveRecord::Migration[6.0]
+  def change
+    add_column :flats, :image_url, :string
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,45 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning database..."
+Flat.destroy_all
+
+puts "create 4 fake flats"
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3,
+)
+
+
+
+Flat.create!(
+  name: 'St Pancras Clock Tower Guest Suite',
+  address: 'Euston Rd London N1C 4QP',
+  description: 'close to the station',
+  price_per_night: 110,
+  number_of_guests: 2,
+)
+
+Flat.create!(
+  name: 'Flat in London',
+  address: '15 High Park London Z7 2DR',
+  description: 'Great!',
+  price_per_night: 200,
+  number_of_guests: 5,
+)
+
+Flat.create!(
+  name: 'Coutryside house',
+  address: '7 rue du Magoula, Turny',
+  description: 'A lovely house in Burgundy',
+  price_per_night: 50,
+  number_of_guests: 10,
+  image_url: "https://source.unsplash.com/9DDYhJ7LwZA/1600x900"
+)
+
+puts "done"
